60dcb0c9e0b763fd65d41c9729dd9f13a6846cf8
jitu <iamjitu.pradhan@gmail.com>
2021-08-20 13:37:26 +0530

    commit

--- recursion/src/PrintN.java
+++ recursion/src/PrintN.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class PrintN {
+
+    public static void print(int n){
+        if(n==0){
+            System.out.println(n+" ");
+            return;
+        }
+        print(n-1);
+        System.out.println(n+" ");
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        print(5);
+
+    }
 }
